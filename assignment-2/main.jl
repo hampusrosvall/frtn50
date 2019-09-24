8e78aa640e39f24ccf5e808488214a8d23adf99e
LurreMcFly <jonaslundgren94@gmail.com>
2019-09-24 11:52:49 +0200

    task 2 done ish

--- assignment-2/main.jl
+++ assignment-2/main.jl
@@ -17,55 +17,63 @@ end
 
 plot!(p,x,min_max_scale(y), seriestype =:scatter)
 
-function least_squares_prox(x, y_unscaled; it = 1000, tol = 10e-6)
-    p = size(x[1,:])
-    w = randn(p)
-    y = min_max_scale(y_unscaled)
-    f = SqrNormL2()
-
-    gam = 1/eigmax(x * x')
-
-    for i = 1:1000
-        z = w .- gam.*(x'*(x*w - y))
-        w_prev = w
-        prox!(w, f, z, gam)
-
-        #OM VI VILL KÖRA MED TOL ISTÄLLET
-        #if norm(w_prev - w) < tol
-        #    print(i)
-        #    break
-        #end
-    end
-    return w
-end
-
-w = least_squares_prox(x,y, it = 10000000)
-
-p = plot()
-plot!(p, x, y_scaled, seriestype =:scatter)
-plot!(p, x, x.*w)
-
 function expand_x(x, p)
     x_expand = ones(length(x), p + 1)
-    for i = 2:(p + 1)
-        x_expand[:, i] = x.^(i - 1)
+    for i = 1:p
+        x_expand[:, i + 1] = x.^i
     end
     return x_expand
 end
 
-function plot_ploynomials(x,y,p)
-    x_expand = expand_x(x, p)
+function least_squares_gd(x_unexpanded, y_unscaled; p = 1, it = 1000, tol = 10e-10, do_plot = false)
+    X = expand_x(x_unexpanded, p)
+    w = randn(p+1)
+    Y = min_max_scale(y_unscaled)
+    f = LeastSquares(X, Y)
+
+    gam = 1/eigmax(X * X')
+    conv_iter = 0
+
+    for i = 1:it
+        w_prev = copy(w)
+        gradfw, _ = gradient(f, w)
+        w = w - gam * gradfw
+
+        if norm(w_prev - w) < tol
+            conv_iter = i
+            break
+        end
+    end
+
+    if do_plot
+        pl = plot()
+        plot!(pl, x_unexpanded, Y, seriestype =:scatter)
+        plot!(pl, x_unexpanded, X * w)
+        display(pl)
+        return
+    end
+    return w, conv_iter
+end
+
+least_squares_gd(x, y, p = 4, do_plot = true)
+
+function plot_polynomials(x, y, p)
     y_scaled = min_max_scale(y)
 
-    pl = plot()
-    x_grid = range(minimum(x), stop = maximum(x), length = 1000)
+    x_grid = range(minimum(x), stop = maximum(x), length = 100)
     x_grid_expand = expand_x(x_grid, p)
-    plot!(pl, x, y_scaled, seriestype =:scatter, ylims = (-1.2,1.2))
-    for i = 1:(p + 1)
-        x_slize = x_expand[:,1:i]
-        w = least_squares_prox(x_slize, y)
-        print(w)
-        plot!(pl, x_grid, x_grid_expand[:,1:i]*w)
+    #pl = plot()
+    #plot!(pl, x, y_scaled, seriestype =:scatter, ylims = (-1.2,1.2))
+
+    for i = 2:(p + 1)
+        w, conv_iter = least_squares_gd(x, y, p = i-1, tol = 10e-17)
+        #plot!(pl, x_grid, x_grid_expand[:,1:i]*w)
+        pl = plot()
+        plot!(pl, x, y_scaled, seriestype =:scatter, ylims = (-1.2,1.2))
+        y_pred = x_grid_expand[:,1:i] * w
+        plot!(pl, x_grid, x_grid_expand[:,1:i] * w)
         display(pl)
     end
 end
+
+plot_polynomials(x,y,10)
