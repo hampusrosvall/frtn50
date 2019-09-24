2f6b6c00840ba91620f6b73fa92f9e1cb8306b04
LurreMcFly <jonaslundgren94@gmail.com>
2019-09-24 14:40:08 +0200

    new commit

--- assignment-2/main.jl
+++ assignment-2/main.jl
@@ -8,11 +8,11 @@ x, y = leastsquares_data()
 p = plot()
 plot!(p,x,y, seriestype =:scatter)
 
-function min_max_scale(y)
-    y_max = maximum(y)
-    y_min = minimum(y)
+function min_max_scale(x)
+    x_max = maximum(x)
+    x_min = minimum(x)
 
-    return 2 * (y .- y_min)./(y_max - y_min) .- 1
+    return 2 * (x .- x_min)./(x_max - x_min) .- 1
 end
 
 plot!(p,x,min_max_scale(y), seriestype =:scatter)
@@ -25,10 +25,11 @@ function expand_x(x, p)
     return x_expand
 end
 
-function least_squares_gd(x_unexpanded, y_unscaled; p = 1, it = 1000, tol = 10e-10, do_plot = false)
+function least_squares_gd(x_unexpanded_unscaled, Y; p = 1, it = 1000, tol = 10e-10, do_plot = false)
+    x_unexpanded = min_max_scale(x_unexpanded_unscaled)
     X = expand_x(x_unexpanded, p)
     w = randn(p+1)
-    Y = min_max_scale(y_unscaled)
+
     f = LeastSquares(X, Y)
 
     gam = 1/eigmax(X * X')
@@ -37,7 +38,7 @@ function least_squares_gd(x_unexpanded, y_unscaled; p = 1, it = 1000, tol = 10e-
     for i = 1:it
         w_prev = copy(w)
         gradfw, _ = gradient(f, w)
-        w = w - gam * gradfw
+        w = w .- gam * gradfw
 
         if norm(w_prev - w) < tol
             conv_iter = i
@@ -55,22 +56,22 @@ function least_squares_gd(x_unexpanded, y_unscaled; p = 1, it = 1000, tol = 10e-
     return w, conv_iter
 end
 
-least_squares_gd(x, y, p = 4, do_plot = true)
+least_squares_gd(x, y, p = 10, do_plot = true)
 
-function plot_polynomials(x, y, p)
-    y_scaled = min_max_scale(y)
-
-    x_grid = range(minimum(x), stop = maximum(x), length = 100)
+function plot_polynomials(x_unscaled, y, p)
+    x_grid = range(-1, stop = 1, length = 100)
     x_grid_expand = expand_x(x_grid, p)
+    x = min_max_scale(x_unscaled)
     #pl = plot()
     #plot!(pl, x, y_scaled, seriestype =:scatter, ylims = (-1.2,1.2))
 
     for i = 2:(p + 1)
-        w, conv_iter = least_squares_gd(x, y, p = i-1, tol = 10e-17)
+        w, conv_iter = least_squares_gd(x_unscaled, y, p = i-1, it = 1000000, tol = 10e-5)
+        print(conv_iter)
+        print("\n")
         #plot!(pl, x_grid, x_grid_expand[:,1:i]*w)
         pl = plot()
-        plot!(pl, x, y_scaled, seriestype =:scatter, ylims = (-1.2,1.2))
-        y_pred = x_grid_expand[:,1:i] * w
+        plot!(pl, x, y, seriestype =:scatter)
         plot!(pl, x_grid, x_grid_expand[:,1:i] * w)
         display(pl)
     end
