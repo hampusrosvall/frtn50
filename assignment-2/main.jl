b5caad7416ec7425a4cf81f33bfaf2a332b407fd
Hampus Rosvall <hampusrosvall@hr.local>
2019-09-27 10:06:21 +0200

    task 7 done

--- assignment-2/main.jl
+++ assignment-2/main.jl
@@ -1,6 +1,7 @@
 using ProximalOperators
 using Plots
 using LinearAlgebra
+using Statistics
 
 include("./problem.jl")
 
@@ -71,20 +72,23 @@ end
 
 plot_polynomials(x,y,15)
 
-function least_squares_reg(X, Y; q = 2, p = 10, lam = 0.1, it = 10000, do_plot = false)
-    # Initializing plot data
-    X_grid = range(minimum(X), stop = maximum(X), length = 100)
-    X_grid_scaled = min_max_scale(X_grid, maximum(X), minimum(X))
-    X_grid_phi = expand_x(X_grid_scaled, p)
+function least_squares_reg(X, Y; q = 2, p = 10, lam = 0.1, it = 10000, do_plot = false, scale = true)
+    if scale
+       X_grid = range(minimum(X), stop = maximum(X), length = 100)
+       X_grid_scaled = min_max_scale(X_grid, maximum(X), minimum(X))
+       X_grid_phi = expand_x(X_grid_scaled, p)
+       X_scaled = min_max_scale(X, maximum(X), minimum(X))
+       X_phi = expand_x(X_scaled, p)
+   else
 
-    # Initializing data
-    X_scaled = min_max_scale(X, maximum(X), minimum(X))
-    X_phi = expand_x(X_scaled, p)
 
     # Initializing functions
     f = LeastSquares(X_phi, Y)
 
     gam = 1/eigmax(X_phi' * X_phi)
+
+    pl = plot()
+    plot!(pl, X, Y, seriestype =:scatter, label = 'Data')
 
     # Proximal gradient descent
     for l in lam
@@ -98,12 +102,110 @@ function least_squares_reg(X, Y; q = 2, p = 10, lam = 0.1, it = 10000, do_plot =
             w, _ = prox(g, w_step, gam)
         end
         if do_plot
-            pl = plot()
-            plot!(pl, X, Y, seriestype =:scatter)
-            plot!(pl, X_grid, X_grid_phi * w)
-            display(pl)
+            plot!(pl, X_grid, X_grid_phi * w, label = l)
+        end
+    end
+    display(pl)
+end
+
+lam_grid = range(0.001, stop = 10, length = 5)
+least_squares_reg(x, y, lam = lam_grid, do_plot = true)
+
+function create_kernel(X; sigma = 1)
+    dim = length(X)
+    K = zeros(dim, dim)
+    for (i, x_i) in enumerate(X)
+        for (j, x_j) in enumerate(X)
+            exponent = -(1 / 2*sigma^2) * norm(x_i - x_j)^2
+            K[i, j] = exp(exponent)
+        end
+    end
+    return K
+end
+
+function svm_dual_solver(X, Y; lam = 0.01, it = 100000, sigma = 1)
+    K = create_kernel(X, sigma = sigma)
+    Q = (1/lam) * diagm(Y) * K * diagm(Y)
+    N = length(X)
+    gam = 1 / eigmax(Q)
+
+    # Initialize functions
+    h = HingeLoss(ones(N), 1/N)
+    h_conj = Conjugate(h)
+    v = randn(length(X))
+    q = zeros(N)
+    g_conj = Quadratic(Q, q)
+
+    for i = 1:it
+        # Perform proximal gradient descent step
+
+        # gradient of quadratic function
+        gradgv, _ = gradient(g_conj, v)
+
+        # update dual parameter mu
+        v = v - gam * gradgv
+
+        # calculate new point v
+        v, _ = prox(h_conj, v, gam)
+    end
+    return v
+end
+
+function create_kernel_vector(X_test, X, sigma)
+    K = zeros(length(X))
+    for i in eachindex(X)
+        K[i] = exp(-(1/(2*sigma^2)) * norm(X[i] - X_test)^2)
+    end
+    return K
+end
+
+
+function predict(X_test, v, Y, X; lambda = 1, sigma = 1)
+    Y_pred = zeros(length(X_test))
+
+    for i in eachindex(X_test)
+        K = create_kernel_vector(X_test[i], X, sigma)
+        Y_pred[i] = sign((-1/lambda) * transpose(v) * diagm(Y) * K)
+    end
+    return Y_pred
+end
+
+sigma = 0.3
+# Initialize training data
+X, Y = svm_train()
+
+# Solve the dual problem
+v = svm_dual_solver(X, Y, lam = 0.01, sigma = sigma)
+
+function grid_search(lambda_grid, sigma_grid)
+    X, Y = svm_train()
+    X_test, Y_test = svm_test_1()
+    print("============= GRID SEARCH =============\n")
+    for lam in lambda_grid
+        for sigma in sigma_grid
+            # Solve the dual problem
+            v = svm_dual_solver(X, Y, lam = lam, sigma = sigma)
+
+            # Make predictions on test data
+            Y_pred_test = predict(X_test, v, Y, X, lambda = lam, sigma = sigma)
+
+            # Calculate error rate on test data
+            error_test = mean(Y_pred_test .!= Y_test)
+
+            # Predict on training data
+            Y_pred_train = predict(X, v, Y, X, lambda = lam, sigma = sigma)
+
+            # Calculate error rate on train data
+            error_train = mean(Y_pred_train .!= Y)
+
+            # Print outs
+            print("Lambda: ", lam, "Sigma: ", sigma, "Test error rate: ", error_test,
+                            "Train error rate: ", error_train, "\n")
         end
     end
 end
 
-least_squares_reg(x, y, lam = 0.1, do_plot = true)
+lambda_grid = [0.1, 0.01, 0.001, 0.0001]
+sigma_grid = [1, 0.5, 0.25]
+
+grid_search(lambda_grid, sigma_grid)
